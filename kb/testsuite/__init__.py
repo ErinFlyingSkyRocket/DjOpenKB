1fa4a1cd5bb3db495bfbe1d06ba97feae22a0063
ErinFlyingSkyRocket <erinflyingskyrocket@gmail.com>
2026-06-15 00:28:58 +0800

    Revert "thorough_tests_patch"
    This reverts commit ff1a956da43a0f7b81fb8f9bc854b8cc99d981e5.

--- kb/testsuite/__init__.py
+++
@@ -1,8 +0,0 @@
-"""DjOpenKB regression/security test package.
-
-Run with:
-    python manage.py test kb --verbosity=2
-
-The test modules are split by concern so future role/workflow changes can be
-checked without manually clicking through the whole site.
-"""
